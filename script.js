3e1fe15e4597af0d3302a7229bd88d79a9b5ae20
Pawel-Michalski <p.michalski@kontakt.io>
2022-10-21 11:06:27 +0200

    add timezone select, change startTime to endTime

--- script.js
+++ script.js
@@ -12,34 +12,76 @@ let buttonDownload = document.querySelector("#download")
 let buttonFetch = document.querySelector("#fetch")
 let spinner = document.querySelector(".lds-roller")
 let apiKeyInput = document.querySelector("#apiKey-input")
+let timezoneCST = document.querySelector("#cst")
+let timezoneUTC = document.querySelector("#utc")
+let timezoneCDT = document.querySelector("#cdt")
+let timezoneSelectDiv = document.querySelector(".timezone-select")
+let timezoneMessage = document.querySelector("#timezone-message")
+let timezoneSelected = "utc";
+
+
 
 function download_csv_file() {
-
+    let timeOffset = 0;
     //define the heading for each row of the data
-    let csv = 'mac,last seen,last location,uniqueId,alias\n';
+    //let csv = 'mac,last seen,last location,uniqueId,alias\n';
 
     //merge the data with CSV
 
     let csvContent = "data:text/csv;charset=utf-8,";
     csvContent += 'mac,last seen,last location,uniqueId,name,firmware\n';
     results.forEach(function (rowArray) {
+        let dateToBeConverted = new Date(rowArray[1])
+        //console.log(dateToBeConverted, "dateToBeConverted")
+        let currentTimeUTC = new Date(Date.UTC())
+
+        let unixDateToBeConverted = Math.floor(dateToBeConverted.getTime() / 1000);
+        let unixDateAfterConversion = 0;
+        //console.log(unixDateToBeConverted, "unixDateToBeConverted")
+        //console.log(timezoneSelected)
+
+        if (timezoneSelected === "cst") {
+            timeOffset = -21600;
+        } else if (timezoneSelected === "cdt") {
+            timeOffset = -18000;
+        }
+        //console.log(timeOffset, "timeOffset")
+        unixDateAfterConversion = unixDateToBeConverted + timeOffset;
+        let dateAfterConversion = new Date(unixDateAfterConversion * 1000);
+        let year = dateAfterConversion.getFullYear();
+        let month = dateAfterConversion.getMonth() + 1;
+        if (month < 10) month = '0' + month
+        let day = dateAfterConversion.getDate();
+        if (day < 10) day = '0' + day;
+        let hour = dateAfterConversion.getHours();
+        if (hour < 10) hour = '0' + hour;
+        let minute = dateAfterConversion.getMinutes()
+        if (minute < 10) minute = '0' + minute;
+        let second = dateAfterConversion.getSeconds()
+        if (second < 10) second = '0' + second
+        let dateAfterConversionFormatted = `${year}-${month}-${day}T${hour}:${minute}:${second}`
+        //console.log(unixDateAfterConversion, "unixDateAfterConversion")
+        //console.log(dateAfterConversion, "dateAfterConversion")
+        //console.log(dateAfterConversionFormatted, "dateAfterConversionFormatted")
+        //let stringDateAfterConversion = dateAfterConversion.toString();
+        //console.log(stringDateAfterConversion, "stringDateAfterConversion")
+        rowArray[1] = dateAfterConversionFormatted
         let row = rowArray.join(",");
         csvContent += row + "\r\n";
     });
     var encodedUri = encodeURI(csvContent);
     window.open(encodedUri);
-    console.log(row)
 }
 function showResults() {
-    console.log(results, "results")
+    //console.log(results, "results")
     display.innerText = `data ready to download`
-    console.log(results.length, "results.length")
+    //console.log(results.length, "results.length")
     buttonDownload.style.display = "block"
     spinner.style.display = "none"
 }
 function getPresences() {
     // fetch presence for each device and save as object in array
-    console.log(macAddresses.length)
+    //console.log(macAddresses.length)
     macAddresses.forEach(device => {
         var myHeaders = new Headers();
         myHeaders.append("Api-Key", apiKey);
@@ -54,7 +96,29 @@ function getPresences() {
         fetch(`https://apps.cloud.us.kontakt.io/v3/presences?trackingId=${device.mac.toLowerCase()}`, requestOptions)
             .then(response => response.json())
             .then(result => {
-                results.push([result.content[0].trackingId, result.content[0].startTime.slice(0, 19), result.content[0].roomName, device.uniqueId, device.name, device.firmware])
+                let endTime = '';
+                //console.log(result.content[0]);
+                //console.log(result.content[0].endTime);
+                if (result.content[0].endTime == undefined) {
+                    let currentTime = new Date();
+                    //console.log(currentTime, "currenttime");
+                    let year = currentTime.getUTCFullYear();
+                    let month = currentTime.getUTCMonth() + 1;
+                    if (month < 10) month = '0' + month
+                    let day = currentTime.getUTCDate();
+                    if (day < 10) day = '0' + day;
+                    let hour = currentTime.getUTCHours();
+                    if (hour < 10) hour = '0' + hour;
+                    let minute = currentTime.getUTCMinutes()
+                    if (minute < 10) minute = '0' + minute;
+                    let second = currentTime.getUTCSeconds()
+                    if (second < 10) second = '0' + second
+                    endTime = `${year}-${month}-${day}T${hour}:${minute}:${second}`
+                } else {
+                    endTime = result.content[0].endTime.slice(0, 19)
+                }
+                //console.log(endTime);
+                results.push([result.content[0].trackingId, endTime, result.content[0].roomName, device.uniqueId, device.name, device.firmware])
                 //device.alias
                 display.innerText = `found positions of ${results.length} badges. Please wait...`
             })
@@ -64,15 +128,15 @@ function getPresences() {
 };
 
 const findMacs = (result) => {
-    console.log(result.devices)
+    //console.log(result.devices)
     result.devices.forEach(device => {
         if (device.product === "Smart Badge") {
-            console.log(device.mac)
+            //console.log(device.mac)
 
             macAddresses.push({ "mac": device.mac, "uniqueId": device.uniqueId, "name": device.name, "firmware": device.firmware })
         }
     })
-    console.log(macAddresses) //all badges here
+    //console.log(macAddresses) //all badges here
     display.innerText = `found ${macAddresses.length} badges, fetching positions...`
 }
 
@@ -108,7 +172,7 @@ const setNumberOfBadges = (response) => {
     numberOfBadges = response.searchMeta.count
     pages = numberOfBadges / 500
     pages = Math.floor(pages) + 1;
-    console.log(pages)
+    //console.log(pages)
     fetchMacs();
 }
 const checkNumberOfBadges = () => {
@@ -123,7 +187,7 @@ const checkNumberOfBadges = () => {
 
     fetch('https://api.kontakt.io/device?maxResult=5&queryType=COUNTED', options)
         .then(response => {
-            console.log(response)
+            //console.log(response)
             if (response.status >= 300) {
                 display.innerText = `response status: ${response.status}. Most likely apiKey is incorrect. Refresh the page and try again`
                 spinner.style.display = "none"
@@ -139,11 +203,35 @@ const healthCheck = () => {
     if (apiKeyInput.value.length > 0) {
         apiKey = apiKeyInput.value
         checkNumberOfBadges();
+        timezoneSelectDiv.style.display = "none";
         apiKeyInput.style.display = 'none';
         // apiKeyInput.style.display = 'none';
         buttonFetch.style.display = 'none';
         spinner.style.display = "inline-block";
+        timezoneMessage.style.display = "none";
     } else {
         display.innerText = `input apiKey first`
     }
 }
+
+
+
+timezoneUTC.addEventListener("click", function (e) {
+    timezoneSelected = "utc"
+    timezoneUTC.classList.add("glowing-border")
+    timezoneCST.classList.remove("glowing-border")
+    timezoneCDT.classList.remove("glowing-border")
+})
+timezoneCST.addEventListener("click", function (e) {
+    timezoneSelected = "cst"
+    timezoneCST.classList.add("glowing-border")
+    timezoneUTC.classList.remove("glowing-border")
+    timezoneCDT.classList.remove("glowing-border")
+})
+timezoneCDT.addEventListener("click", function (e) {
+    timezoneSelected = "cdt"
+    timezoneCDT.classList.add("glowing-border")
+    timezoneCST.classList.remove("glowing-border")
+    timezoneUTC.classList.remove("glowing-border")
+})
+timezoneUTC.classList.add("glowing-border")
